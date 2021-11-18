cdb419964754011ab03997cfa20ba8123f4bdc41
Lucas Barros Santos <lucasbarrossantos@MacBook-Pro-de-Lucas.local>
2021-11-18 17:52:31 -0300

    Processo CI/CD

--- scripts/start-process.sh
+++ scripts/start-process.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar /tmp/artifact_name.jar > /dev/null 2 > /dev/null < /dev/null &
+java -jar /tmp/estudoapp.jar > /dev/null 2 > /dev/null < /dev/null &
--- scripts/stop-process.sh
+++ scripts/stop-process.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ps -ef | grep artifact_name.jar | grep -v grep | awk '{print $2}' | xargs kill
+ps -ef | grep estudoapp.jar | grep -v grep | awk '{print $2}' | xargs kill
