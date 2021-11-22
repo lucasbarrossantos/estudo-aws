adbb6a4b1bd5338c1209abd7f957b89f41ddf6c9
Lucas Barros Santos <lucasbarrossantos@MacBook-Pro-de-Lucas.local>
2021-11-21 23:28:18 -0300

    Estudo AWS Pipeline

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo yum install java-11-amazon-corretto-headless -y
-pwd
-ls -la
+
+cd /home/ec2-user/app
 sudo ./mvnw clean install
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd /target
+cd /home/ec2-user/app/target
 sudo java -jar estudoapp.jar >/dev/null 2>&1 &
