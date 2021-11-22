6d54e8f8ec1650c2144604d01d13eb4270b77d20
Lucas Barros Santos <lucasbarrossantos@MacBook-Pro-de-Lucas.local>
2021-11-22 16:50:59 -0300

    Estudo AWS

--- src/main/java/com/estudo/EstudoApplication.java
+++ src/main/java/com/estudo/EstudoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @RestController
 @SpringBootApplication
 public class EstudoApplication {
@@ -16,6 +19,7 @@ public class EstudoApplication {
 
 	@GetMapping("/health-check")
 	public ResponseEntity<String> healthCheck() {
+		System.out.println("Atualizado em: " + LocalDateTime.now());
 		return ResponseEntity.ok("No ar!!!");
 	}
 
